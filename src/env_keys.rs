7df4cedf2d0b7519718160856a60ff02fe2e4d5f
dj8yf0μl <noreply@nowhere.org>
2025-03-25 19:14:32 +0200

    move in [env_keys::nep330] as [env_keys]

---
+++ src/env_keys.rs
@@ -0,0 +1,23 @@
+// ====================== NEP-330 1.2.0 - Build Details Extension ===========
+/// NEP-330 1.2.0
+pub const BUILD_ENVIRONMENT: &str = "NEP330_BUILD_INFO_BUILD_ENVIRONMENT";
+/// NEP-330 1.2.0
+pub const BUILD_COMMAND: &str = "NEP330_BUILD_INFO_BUILD_COMMAND";
+/// NEP-330 1.2.0
+pub const CONTRACT_PATH: &str = "NEP330_BUILD_INFO_CONTRACT_PATH";
+/// NEP-330 1.2.0
+pub const SOURCE_CODE_SNAPSHOT: &str = "NEP330_BUILD_INFO_SOURCE_CODE_SNAPSHOT";
+// ====================== End section =======================================
+
+// ====================== NEP-330 1.1.0 - Contract Metadata Extension ===========
+/// NEP-330 1.1.0
+pub const LINK: &str = "NEP330_LINK";
+/// NEP-330 1.1.0
+pub const VERSION: &str = "NEP330_VERSION";
+// ====================== End section =======================================
+/// TODO #A: make pub(crate)
+pub mod nonspec {
+    pub const SERVER_DISABLE_INTERACTIVE: &str = "CARGO_NEAR_SERVER_BUILD_DISABLE_INTERACTIVE";
+}
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
-pub mod types;
+/// TODO #B: move to logic module 
 pub mod docker_checks;
+/// TODO #B: move to logic module 
 pub mod docker_command;
+pub mod types;
+
+mod logic {
+    pub fn shell_escape_nep330_build_command(build_command: Vec<String>) -> String {
+        tracing::debug!("cli_build_command_in_docker {:#?}", build_command);
+        shell_words::join(build_command)
+    }
+}
 
 
-pub mod nep330;
+
+/// module contains variables, which are set to configure build with WASM reproducibility,
+/// which correspond to some fields of `ContractSourceMetadata` in <https://github.com/near/NEPs/blob/master/neps/nep-0330.md>
+pub mod env_keys;
 
 
-
+pub use logic::shell_escape_nep330_build_command;
