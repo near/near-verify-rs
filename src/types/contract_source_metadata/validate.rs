f336224fcd3a69ae2ee0bc4d7b7598eaa83be3be
dj8yf0μl <noreply@nowhere.org>
2025-03-26 22:50:38 +0200

    eextract a [BuildInfo::validate_contract_path] method

--- src/types/contract_source_metadata/validate.rs
+++ src/types/contract_source_metadata/validate.rs
@@ -3,7 +3,6 @@ use std::str::FromStr;
 /// TODO #H4: add validation of [BuildInfoMixed::build_environment] with `images_whitelist` [Vec<String>] argument
 /// TODO #H3: check [BuildInfoMixed::build_environment] for regex match
 /// TODO #H2: add validation for `build_command`, that the vec isn't empty, and all tokens aren't empty
-/// TODO #C1: extract a [ContractSourceMetadata::validate_contract_path] method
 impl super::ContractSourceMetadata {
     pub fn validate(&self) -> eyre::Result<()> {
         if self.build_info.is_none() {
@@ -13,11 +12,20 @@ impl super::ContractSourceMetadata {
         }
 
         let build_info = self.build_info.as_ref().unwrap();
-        match unix_path::PathBuf::from_str(&build_info.contract_path) {
+
+        build_info.validate_contract_path()?;
+
+        Ok(())
+    }
+}
+
+impl super::build_info::BuildInfo {
+    pub fn validate_contract_path(&self) -> eyre::Result<()> {
+        match unix_path::PathBuf::from_str(&self.contract_path) {
             Err(err) => {
                 return Err(eyre::eyre!(
                     "`contract_path` field (`{}`) of `BuildInfo` isn't a valid unix path: {:#?}",
-                    build_info.contract_path,
+                    self.contract_path,
                     err,
                 ));
             }
@@ -25,7 +33,7 @@ impl super::ContractSourceMetadata {
                 if !path.is_relative() {
                     return Err(eyre::eyre!(
                         "`contract_path` field (`{}`) of `BuildInfo` isn't a relative unix path",
-                        build_info.contract_path,
+                        self.contract_path,
                     ));
                 }
                 for component in path.components() {
@@ -34,7 +42,7 @@ impl super::ContractSourceMetadata {
                         // this is somewhat impossible to reach, as the whole path was parsed from a [String]
                         return Err(eyre::eyre!(
                             "`contract_path` field (`{}`) of `BuildInfo` contains a component which is not a valid utf8 string: `{:?}",
-                            build_info.contract_path,
+                            self.contract_path,
                             err,
                         ));
                     }
